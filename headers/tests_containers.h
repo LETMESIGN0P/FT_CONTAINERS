b2160dd8e1887caadc45bacf2c3c7f4ec94a331c
LETMESIGN0P <lrichard@student.42lyon.fr>
2023-03-02 01:49:28 +0100

    asdf

--- headers/tests_containers.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-bool v_test();
-bool m_test();
-bool s_test();
--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include "includes/vector.h"
-#include <vector>
-
-int main()
-{
-	std::vector<int> v(3,0);
-	std::vector<int>::iterator i = 1 + v.begin();
-	return 0;
-}
